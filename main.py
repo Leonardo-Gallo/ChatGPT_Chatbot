293aa564f5e8249a766b40263b93ddecdd0d1ae9
Leonardo-Gallo <leonardogallo380@gmail.com>
2024-02-21 15:42:07 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from openai import OpenAI
+import sounddevice
+import speech_recognition as sr
+from gtts import gTTS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key = os.getenv("OPENAI_KEY")
+    )
+
+def generate_response(prompt):
+    response = client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=150
+            )
+    return response.choices[0].text.strip()
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone(device_index=3) as source:
+        print("say something...")
+        audio = recognizer.listen(source, timeout=1)
+    try:
+        user_input = recognizer.recognize_google(audio)
+        print(f"You: {user_input}")
+        return user_input
+    except sr.UnknownValueError:
+        print("Sorry I couldn't understand that")
+        return ""
+    except sr.RequestError as e:
+        print(f"Speech recognition request failed: {e}")
+        return ""
+
+def text_to_speech(text):
+    tts = gTTS(text)
+    tts.save("response.mp3")
+    os.system("mpg321 response.mp3")
+
+def main():
+    print("welcome to my chatbot!")
+
+    while True:
+        user_input = recognize_speech()
+
+        if not user_input:
+            continue
+        if user_input.lower() in ['exit','quit']:
+            print("goodbye..")
+            break
+        
+        prompt = f"You: {user_input}\nGPT-3: "
+        response = generate_response(prompt)
+
+        print(f"GPT-3: {response}")
+        text_to_speech(response)
+
+if __name__ == "__main__":
+    main()
